363d12fc64107307380bed84f76acf980c321959
kmgmedia <hellokmgmedia@gmail.com>
2024-02-05 02:46:14 +0100

    More depens

---
+++ Backend-folder/route/auth.js
@@ -0,0 +1,8 @@
+const Joi = require('joi');
+const { User } = require("../model/user")
+
+
+
+
+
+module.exports = router
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
+const config = require('config');
 
 const mongoose = require('mongoose');
 const express = require('express');
@@ -12,3 +13,13 @@ const userRouter = require('./route/users')
 mongoose.connect('mongodb://127.0.0.1:27017/bycapp')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.log('Cannot Connect to MongoDB...', err))
+
+
+
+app.use(express.json());
+app.use('/api/user', userRouter);
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {console.log(`listening on port ${port}...`)})
